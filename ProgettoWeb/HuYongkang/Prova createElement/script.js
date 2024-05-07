250600894750fa932e0e7aca892c42e45aee46a6
YongkangHu <121815766+yongkang49@users.noreply.github.com>
2024-05-07 09:15:02 +0200

    Prova createElement

---
+++ ProgettoWeb/HuYongkang/Prova createElement/script.js
@@ -0,0 +1,6 @@
+let p = document.createElement("p");
+p.textContent = "informatica";
+p.classList = "borderP";
+let div = document.querySelector("div");
+//div.appendChild(p);
+document.body.insertBefore(p,document.body.querySelector("script"));
