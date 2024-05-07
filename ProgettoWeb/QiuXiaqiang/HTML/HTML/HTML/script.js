bb09c3f1140bbbad7e05fdfa2c95251b0134d5f7
Xiaqiang Qiu <122173121+Qiu-Xiaqiang@users.noreply.github.com>
2024-05-07 09:15:11 +0200

    Aggiunto il lavoro svolto in classe

---
+++ ProgettoWeb/QiuXiaqiang/HTML/HTML/HTML/script.js
@@ -0,0 +1,7 @@
+let p = document.createElement("p");
+p.textContent = "Informatica";
+p.classList = "borderP";
+//document.body.appendChild(p);
+//let div = document.body.querySelector("div");
+//div.appendChild(p);
+document.body.insertBefore(p,document.body.querySelector("script"));
