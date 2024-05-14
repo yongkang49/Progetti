97694ab568a9df5b4f71a5507f06f18e53d85864
Xiaqiang Qiu <122173121+Qiu-Xiaqiang@users.noreply.github.com>
2024-05-14 09:23:00 +0200

    Aggiunto yuan

---
+++ ProgettoWeb/QiuXiaqiang/HTML/HTML/YuanShen/script.js
@@ -0,0 +1,40 @@
+const btn = document.body.querySelector("button");
+btn.addEventListener("click", function()
+{
+    const width = 600;
+    const height = 400;
+    const left = (screen.width - width) / 2;
+    const top = (screen.height - height) / 2;
+    let newwindow = document.open("", "", "width ="+ width +","+ "height =" + height +","+ "left =" + left +","+ "top =" + top);
+    
+    
+    let style = newwindow.document.createElement("link")
+    style.rel = "stylesheet"
+    style.href = "style.css"
+    newwindow.document.head.appendChild(style);
+
+    let form = newwindow.document.createElement("form");
+    form.classList = "container"
+
+    let user = newwindow.document.createElement("input");
+    user.type = "text";
+    user.placeholder = "Nome località";
+    user.required = true;
+    let pwd = newwindow.document.createElement("input");
+    pwd.type = "password";
+    pwd.placeholder = "Personaggio";
+    pwd.required = true;
+    let submit = newwindow.document.createElement("input");
+    submit.type = "submit";
+    submit.value = "invia dati";
+     
+    form.appendChild(user);
+    form.appendChild(pwd);
+    form.appendChild(submit);
+    newwindow.document.body.appendChild(form);
+    let script2 = newwindow.document.createElement("script");
+    script2.src = "script2.js";
+    newwindow.document.body.appendChild(script2);
+}) 
+const par = document.body.querySelector("p");
+par.textContent = localStorage.getItem("username") + "   " + localStorage.getItem("password");
