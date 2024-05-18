1d49416e8039f8f7ceb1ca0bc1c49aa5f3608df9
Hu Yongkang <yongkang.hu@iisviolamarchesini.edu.it>
2024-05-18 10:11:19 +0200

    eliminato codice inutili

--- ProgettoWeb/HuYongkang/script.js
+++ ProgettoWeb/HuYongkang/script.js
@@ -14,19 +14,3 @@ function Change() {
         }    
     setTimeout(Change, 4000);
 }
-/*
-let t = ["https://cdn2.unrealengine.com/genshin-impact-4-1920x1080-30df09f89d45.jpg",
-    
-]
-//``
-console.log(`"url('${t[0]}')"`);
-document.querySelector("body").style.backgroundImage = (`"url('${t[0]}')"`);
-ChangeBack();
-function ChangeBack() {
-    myIndex++;
-    if (myIndex > t.length) 
-        {
-            myIndex = 0
-        }    
-    setTimeout(ChangeBack, 4000);
-}*/
